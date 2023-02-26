b0528ccaa349326e7008f4983e4fecb26d23c3f3
AnakinSkywalkeer <gurmanavdeepsinghmahal9@gmail.com>
2023-02-26 18:24:17 +0530

    removed the memory storage and implemented the mongoDB data base

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // represents the data about a record album
@@ -14,38 +21,85 @@ type album struct {
 	Price  float64 `json:"Price"`
 }
 
-// albums slice to seed record album data
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	var results []album
+	client := ConnectDB()
+	collection := GetCollection(client)
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+	if err := cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	c.IndentedJSON(http.StatusOK, results)
 }
+
 func postAlbums(c *gin.Context) {
+	client := ConnectDB()
+	collection := GetCollection(client)
 	var newAlbum album
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	_, err := collection.InsertOne(ctx, newAlbum)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusConflict, newAlbum)
+	} else {
+		c.IndentedJSON(http.StatusCreated, newAlbum)
+	}
+
 }
 func getusingID(c *gin.Context) {
 	id := c.Param("id")
+	var results []album
+	client := ConnectDB()
+	collection := GetCollection(client)
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		panic(err)
+	}
+	if err := cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
 
-	for _, a := range albums {
+	for _, a := range results {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+
 	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+func ConnectDB() *mongo.Client {
+	Mongo_URL := "mongodb://127.0.0.1:27017"
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongo_URL))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	defer cancel()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to mongoDB")
+	return client
+}
+func GetCollection(client *mongo.Client) *mongo.Collection {
+	collection := client.Database("local").Collection("new")
+	return collection
 }
 func main() {
+
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getusingID)
